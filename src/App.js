b312a5b4a0f276d5c6a9a434577efe96210834a9
koushith <koushith97@gmail.com>
2020-05-16 20:40:08 +0530

    Converted to Functional Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import Advice from './components/Advice';
+import Footer from './components/Footer';
 import './App.css';
 
 const App = () => {
-  const [advices, setAdvices] = useState([]);
+  const [advices, setAdvices] = useState();
   const fetchAdvice = async () => {
     const res = await Axios.get('https://api.adviceslip.com/advice');
     console.log('RESPONSE: ', res.data.slip.advice);
@@ -18,12 +19,17 @@ const App = () => {
     fetchAdvice();
   }, []);
   return (
-    <div className='container-fluid app'>
-      <div className='card p-4'>
-        <Advice advices={advices} />
-        <button className='btn btn-primary mt-3 button' onClick={fetchAdvice}>
-          GIVE ME MORE ADVICE!!
-        </button>
+    <div className='app'>
+      <div className='container-fluid '>
+        <div className='card p-4'>
+          <Advice advices={advices} />
+          <button className='btn btn-primary mt-3 button' onClick={fetchAdvice}>
+            GIVE ME MORE ADVICE!!
+          </button>
+        </div>
+      </div>
+      <div className='footer-credits'>
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <footer class='footer footer-credits'>
+        <div class='container'>
+          <span class='text-muted'>&copy; App by Koushith</span>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
