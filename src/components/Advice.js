dcaa9aaf94325c2ab37b13d1f8f39a310ed8268b
koushith <koushith97@gmail.com>
2020-05-23 03:04:47 +0530

    updated

--- src/components/Advice.js
+++ src/components/Advice.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
+import spinner from '../images/spinner.svg';
 
 const Advice = () => {
   const [advices, setAdvices] = useState();
+  const [loading, setLoading] = useState(true);
+
   const fetchAdvice = async () => {
+    setLoading(true);
     const res = await Axios.get('https://api.adviceslip.com/advice');
     console.log('RESPONSE: ', res.data.slip.advice);
 
     const ad = res.data.slip.advice;
-
+    setLoading(false);
     setAdvices(ad);
   };
   // load before hitting button
@@ -19,7 +23,8 @@ const Advice = () => {
     <div className='app'>
       <div className='container-fluid '>
         <div className='card p-4'>
-          <h1>{advices} </h1>
+          {loading ? <img src={spinner} alt='loading' /> : <h1>{advices} </h1>}
+
           <button className='btn btn-primary mt-3 button' onClick={fetchAdvice}>
             GIVE ME MORE ADVICE!!
           </button>
--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
+import spinner from '../images/spinner.svg';
 
 const Joke = () => {
   const [jokes, setJokes] = useState();
+  const [loading, setLoading] = useState(true);
   const fechJokes = async () => {
+    setLoading(true);
     const res = await Axios.get(
       'https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/type/general'
     );
-    console.log('RESPONSE: ', res);
+    console.log('RESPONSE: ', res.data[0].punchline);
 
     const joke = `${res.data[0].setup} ${res.data[0].punchline}`;
 
     setJokes(joke);
+    setLoading(false);
   };
   // load before hitting button- if we didnt include [] it will run continuesly
   useEffect(() => {
@@ -21,7 +25,8 @@ const Joke = () => {
     <div className='app'>
       <div className='container-fluid '>
         <div className='card p-4'>
-          <h1>{jokes} </h1>
+          {loading ? <img src={spinner} alt='loading' /> : <h1>{jokes} </h1>}
+
           <button className='btn btn-primary mt-3 button' onClick={fechJokes}>
             CRACK ANOTHER ONE!!
           </button>
